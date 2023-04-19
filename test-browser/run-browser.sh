b80f4a6e3bcb188675c828ca362196102d8c2fd8
Jaeho Lee <jaeho@dev.thingd.com>
2023-04-19 17:25:23 +0900

    poc for host-resolver-rules based concurrency

---
+++ test-browser/run-browser.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+"/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary" --host-resolver-rules="MAP dev1-jaeholee.org localhost:3000, MAP dev2-jaeholee.org localhost:3000, MAP dev3-jaeholee.org localhost:3000, MAP dev4-jaeholee.org localhost:3000, MAP dev5-jaeholee.org localhost:3000, MAP dev6-jaeholee.org localhost:3000"
